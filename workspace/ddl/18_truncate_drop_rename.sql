00da4b6389852e58e39a5c17d9ac05e2333d900b
Pinixk <bjkim0110@gmail.com>
2022-06-15 15:19:40 +0900

    06/15

---
+++ workspace/ddl/18_truncate_drop_rename.sql
@@ -0,0 +1,8 @@
+-- 내용 삭제
+truncate table dept2;
+
+-- 테이블 삭제
+drop table dept2;
+
+-- table 이름 변경
+rename dept3 to dept2;
--- workspace/ddl/19_truncate_drop.sql
+++
@@ -1,5 +0,0 @@
--- 내용 삭제
-truncate table dept2;
-
--- 테이블 삭제
-drop table dept2;
