5906cfe529845245f5f5c7e65f529f28161fce01
Pinixk <bjkim0110@gmail.com>
2022-06-16 12:10:33 +0900

    06/16

--- workspace/5_constraints/02_foreign.sql
+++ workspace/5_constraints/02_foreign.sql
@@ -7,14 +7,41 @@ create table dept(
 create table dept(
     dno number(2),
     dname varchar2(14),
-    loc varchar2(2)
+    loc varchar2(2),
     constraint FK_DEPT_LOC foreign key(loc) references loc(loc)
 ); 
 -- foreign key column 레벨 O, table 레벨 O
 
+create table dept(
+    dno number(2),
+    dname varchar2(14),
+    loc varchar2(2) 
+);
+alter table dept add constraint FK_DEPT_LOC foreign key(loc) references loc2(loc);
+
+
+
+
 -- 연결 시킬 키가 primary key여야 한다.
 alter table loc add constraint pk_loc primary key(loc);
 
--- foreign key 추가/삭제
-alter table dept add constraint fk_dept foreign key(loc) references loc(loc);
-alter table dept drop constraint fk_dept;
+-- primary key 삭제
+delete from loc2 where loc=10;
+-- !!! 연결 되어 있는 primary key 중 사용되고 있다면 지울 수 없다 
+
+-- 해결법
+alter table dept add constraint FK_DEPT_LOC foreign key(loc) references loc2(loc) 
+on delete cascade; 
+-- primary key를 삭제 시 연결된 모든 foreign key 같이 삭제
+
+alter table dept add constraint FK_DEPT_LOC foreign key(loc) references loc2(loc) 
+on delete set null;
+-- primary key를 삭제 시 연결된 모든 foreign key 값이 null로 변경
+
+
+
+-- foreign key 추가/이름 변경/삭제
+alter table dept add constraint FK_DEPT_LOC foreign key(loc) references loc(loc);
+alter table dept rename constraint FK_DEPT_LOC to FK_DEPT_LOC2;
+alter table dept drop constraint FK_DEPT_LOC2;
+-- modify 실행 안됨
