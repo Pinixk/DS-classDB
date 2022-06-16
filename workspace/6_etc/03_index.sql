dcafcbe2a361d04c7da958e9853b3d549cac2d27
Pinixk <bjkim0110@gmail.com>
2022-06-16 17:10:01 +0900

    06/16

--- workspace/6_etc/03_index.sql
+++ workspace/6_etc/03_index.sql
@@ -10,8 +10,15 @@ insert into test values(12,'E1');
 insert into test values(15,'A1');
 insert into test values(14,'M1');
 
+
+
 -- 찾기
 select * from test order by idx;
 
-select /*+ index(test idx_num) */ * from test; 
 -- hint, oracle에서만 사용
+select /*+ index(test idx_num) */ * from test; 
+-- 숫자는 그냥 해도 괜찮음
+
+create index idx_test_name on test(name); -- 문자열 index 생성
+select /*+ index(idx_test_name) */ * from test where name > ' ';
+-- 문자는 where name > ' ' 필요
