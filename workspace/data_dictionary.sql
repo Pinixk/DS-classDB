1cac009b940b4fb86f910b735eeb05c616bcf8b9
Pinixk <bjkim0110@gmail.com>
2022-06-16 14:45:00 +0900

    06/16

--- workspace/data_dictionary.sql
+++ workspace/data_dictionary.sql
@@ -1,5 +1,11 @@
+-- 찾아 보기
 select * from tab;
-
 select * from user_tables;
+select * from user_objects;
+select * from user_constraints;
 
-select * from all_objects;
+
+-- 휴지통 보기
+show RECYCLEBIN;
+-- 휴지통 비우기
+purge RECYCLEBIN;
